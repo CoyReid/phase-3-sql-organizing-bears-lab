a1247abc76633dc41ccb43989a5c3ac47579c82b
Coy Reid <coyreid7@gmail.com>
2021-09-01 18:09:31 -0500

    commit tree 1b6b5ca680389dd2ea393cce8a84bea5e4f91e9b

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 10, "M", "brown", "angry", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 11, "M", "brown", "angry", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 14, "F", "brown", "angry", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 1, "M", "brown", "angry", 1);
+INSERT INTO bears VALUES (5, "Melissa", 10, "F", "brown", "angry", 1);
+INSERT INTO bears VALUES (6, "Grinch", 17, "M", "brown", "angry", 0);
+INSERT INTO bears VALUES (7, "Wendy", 15, "F", "brown", "angry", 0);
+INSERT INTO bears VALUES (8, NULL, 12, "M", "brown", "angry", 0);
